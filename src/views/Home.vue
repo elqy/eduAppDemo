aeedd9e4e778bc9331b0675879e1b076e98df6a1
老刘 <liuqinyi@163.com>
2019-12-02 17:38:22 +0800

    添加了每个人的任务

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    
+    安凯：专业列表
+    李莎：畅销好课
+    马如意：账号页面 登录页面
+    丁佳怡：注册页面
+    李尚杰：列表搜索页面
+    王瀚：课程详情页
+    
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
