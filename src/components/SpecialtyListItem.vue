ee5851cc911bcb7c1c4f27a3dabd0d2f1bd6e416
an123321 <ankai_666@163.com>
2019-12-04 22:09:11 +0800

    新增“专业组件的子组件”

---
+++ src/components/SpecialtyListItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <li :id="item.id"><img :src="item.img" />
+  <slot></slot>
+  </li>
+  <!-- <router-link :to="to">
+    <font-awesome-icon :icon="icon" fixed-width />
+    <br />
+    <span>{{ content }}</span>
+  </router-link> -->
+</template>
+
+<script>
+export default {
+  name: 'SpecialtyListItem',
+  props: ['item']
+}
+</script>
+
+<style scoped lang="less">
+  li {
+    width: 25%;
+    float: left;
+    margin: 0.5rem 0;
+    img {
+      width: 2rem;
+      height: 2rem;
+      margin: 0.5rem 0;
+    }
+    p {
+      font-size: 0.8rem;
+    }
+   }
+</style>
