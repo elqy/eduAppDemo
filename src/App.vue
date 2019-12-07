67c868a517b44009ee7edc7b15b03ff7432858ee
老刘 <liuqinyi@163.com>
2019-12-07 19:52:14 +0800

    完成底部的tarbar导航

--- src/App.vue
+++ src/App.vue
@@ -2,37 +2,18 @@
   <div id="app" class="page-tabbar">
     <div class="page-wrap">
       <div class="page-title">慧中IT学院</div>
-      <div>
-        <mt-cell class="page-part" title="当前选中" :value="selected" />
-      </div>
       <router-view></router-view>
-      <mt-tab-container class="page-tabbar-container" v-model="selected">
-        <mt-tab-container-item id="首页">
-          a
-        </mt-tab-container-item>
-        <mt-tab-container-item id="我的学习">
-          b
-        </mt-tab-container-item>
-        <mt-tab-container-item id="帐号">
-          <div class="page-part">
-            c
-          </div>
-          <router-link to="/">
-            <mt-button type="danger" size="large">退出</mt-button>
-          </router-link>
-        </mt-tab-container-item>
-      </mt-tab-container>
     </div>
     <mt-tabbar v-model="selected" fixed>
-      <mt-tab-item id="首页">
+      <mt-tab-item id="/">
         <font-awesome-icon slot="icon" :icon="['fas', 'home']" size="lg"></font-awesome-icon>
         首页
       </mt-tab-item>
-      <mt-tab-item id="我的学习">
+      <mt-tab-item id="/mystudy">
         <font-awesome-icon slot="icon" :icon="['fas', 'user']" size="lg"></font-awesome-icon>
         我的学习
       </mt-tab-item>
-      <mt-tab-item id="帐号">
+      <mt-tab-item id="/zhanghao">
         <font-awesome-icon slot="icon" :icon="['fas', 'book']" size="lg"></font-awesome-icon>
         帐号
       </mt-tab-item>
@@ -44,7 +25,14 @@ export default {
   name: 'App',
   data: function () {
     return {
-      selected: '首页'
+      selected: '/'
+    }
+  },
+  watch: {
+    selected: function (val, oldVal) {
+      this.$router.push({
+        path: val
+      })
     }
   }
 }
@@ -54,11 +42,14 @@ export default {
   #app {
     overflow: hidden;
     height: 100vh;
-
     .page-wrap {
       overflow: auto;
       height: 100%;
-      padding-bottom: 100px;
+      padding-bottom: 10rem;
+      .page-title{
+        line-height: 1.5rem;
+        font-size: 1.5rem;
+      }
     }
   }
 
