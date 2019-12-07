983807a62a8db060c25b5f8747214ff7fe6dbe30
老刘 <liuqinyi@163.com>
2019-12-07 19:29:01 +0800

    使用mint-ui实现了底部的tarbar

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,95 @@
 <template>
-  <div id="app">
-    <div id="header">
-      头部
-    </div>
-
-    <div id="content">
-      <!-- 路由匹配到的组件将显示在这里 -->
+  <div id="app" class="page-tabbar">
+    <div class="page-wrap">
+      <div class="page-title">慧中IT学院</div>
+      <div>
+        <mt-cell class="page-part" title="当前选中" :value="selected" />
+      </div>
       <router-view></router-view>
+      <mt-tab-container class="page-tabbar-container" v-model="selected">
+        <mt-tab-container-item id="首页">
+          a
+        </mt-tab-container-item>
+        <mt-tab-container-item id="我的学习">
+          b
+        </mt-tab-container-item>
+        <mt-tab-container-item id="帐号">
+          <div class="page-part">
+            c
+          </div>
+          <router-link to="/">
+            <mt-button type="danger" size="large">退出</mt-button>
+          </router-link>
+        </mt-tab-container-item>
+      </mt-tab-container>
     </div>
-
-    <div id="footer">
-      底部.
-    </div>
-<!--    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
- -->
+    <mt-tabbar v-model="selected" fixed>
+      <mt-tab-item id="首页">
+        <font-awesome-icon slot="icon" :icon="['fas', 'home']" size="lg"></font-awesome-icon>
+        首页
+      </mt-tab-item>
+      <mt-tab-item id="我的学习">
+        <font-awesome-icon slot="icon" :icon="['fas', 'user']" size="lg"></font-awesome-icon>
+        我的学习
+      </mt-tab-item>
+      <mt-tab-item id="帐号">
+        <font-awesome-icon slot="icon" :icon="['fas', 'book']" size="lg"></font-awesome-icon>
+        帐号
+      </mt-tab-item>
+    </mt-tabbar>
   </div>
 </template>
 <script>
 export default {
   name: 'App',
   data: function () {
-    return {}
+    return {
+      selected: '首页'
+    }
   }
 }
 </script>
 
 <style lang="less">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  #app {
+    overflow: hidden;
+    height: 100vh;
 
-// #nav {
-//   padding: 30px;
+    .page-wrap {
+      overflow: auto;
+      height: 100%;
+      padding-bottom: 100px;
+    }
+  }
 
-//   a {
-//     font-weight: bold;
-//     color: #2c3e50;
+  // #app {
+  //   font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  //   -webkit-font-smoothing: antialiased;
+  //   -moz-osx-font-smoothing: grayscale;
+  //   text-align: center;
+  //   color: #2c3e50;
 
-//     &.router-link-exact-active {
-//       color: #42b983;
-//     }
-//   }
-// }
+  //   #footer {
+  //     .mint-tabbar{
+  //       .mint-tab-item{
+  //         .mint-tab-item-icon {
+
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
+
+  // #nav {
+  //   padding: 30px;
+
+  //   a {
+  //     font-weight: bold;
+  //     color: #2c3e50;
+
+  //     &.router-link-exact-active {
+  //       color: #42b983;
+  //     }
+  //   }
+  // }
 </style>
