a6a8b1e30b7c1758169b838e6cc630cffcde5c2c
老刘 <liuqinyi@163.com>
2019-12-07 21:03:25 +0800

    使用mint-ui的swiper组件初步实现轮播图

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,33 @@
 <template>
-  <div class="home">
-    <ProfessionalList></ProfessionalList>
+  <div id="home">
+    <mt-swipe :auto="4000">
+      <mt-swipe-item>
+        <img src="../assets/images/1.webp" width="100%" />
+      </mt-swipe-item>
+      <mt-swipe-item>
+        <img src="../assets/images/2.webp" width="100%"/>
+      </mt-swipe-item>
+      <mt-swipe-item>
+        <img src="../assets/images/3.webp" width="100%"/>
+      </mt-swipe-item>
+      <mt-swipe-item>
+        <img src="../assets/images/4.webp" width="100%"/>
+      </mt-swipe-item>
+    </mt-swipe>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import ProfessionalList from '@/components/ProfessionalList.vue'
-
 export default {
-  name: 'home',
-  components: {
-    ProfessionalList
-  }
+  name: 'home'
 }
 </script>
+
+<style lang="less">
+  #home{
+    height: 100%;
+    .mint-swipe{
+      height: 12rem;
+    }
+  }
+</style>
