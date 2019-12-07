5a9990f3089e81630cfeaed943dd508b5179668d
老刘 <liuqinyi@163.com>
2019-12-07 19:28:22 +0800

    添加了fontaswesome字体图标

--- src/main.js
+++ src/main.js
@@ -1,9 +1,24 @@
 import Vue from 'vue'
+import MintUI from 'mint-ui'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon, FontAwesomeLayers, FontAwesomeLayersText } from '@fortawesome/vue-fontawesome'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'mint-ui/lib/style.css'
+
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
 
 Vue.config.productionTip = false
+
+library.add(fas, far, fab)
+
+Vue.use(MintUI)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.component('font-awesome-layers', FontAwesomeLayers)
+Vue.component('font-awesome-layers-text', FontAwesomeLayersText)
 
 new Vue({
   router,
