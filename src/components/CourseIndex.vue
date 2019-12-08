991d5bc6e65d304efe3ea8bcd5e4fce7f98e2137
老刘 <liuqinyi@163.com>
2019-12-08 17:02:11 +0800

    课程单项布局

--- src/components/CourseIndex.vue
+++ src/components/CourseIndex.vue
@@ -1,5 +1,5 @@
 <template>
-  <li id="index">
+  <li class="courseindex">
     <img :src="require('../'+imgsrc)" alt="" />
     <p><slot></slot></p>
   </li>
@@ -9,25 +9,36 @@
 export default {
   name: 'index',
   props: ['imgsrc']
+  // data: function () {
+  //   return {
+  //     width: '200%'
+  //   }
+  // }
 }
 </script>
 
 <style lang="less">
-  #index {
-    float: left;
+  .courseindex {
     width: 50vw;
+    // flex-shrink:1;
+    display: flex;
+    // flex-grow:1;
     display: -webkit-flex;
     /* Safari */
     display: flex;
     justify-content: space-around;
     align-items: center;
     flex-direction: column;
-
     img {
+      width: 48vw;
       display: flex;
-      align-items: center;
-      justify-content: center;
-      align-content: space-around;
+      display: inline-block;
+      vertical-align :top
+      //width: 50%;
+      //display: flex;
+      // align-items: center;
+      // justify-content: center;
+      // align-content: space-around;
     }
 
     p {
