52b503bc67b194dd5c409dee9fd771d02b43fc15
老刘 <liuqinyi@163.com>
2019-12-08 16:31:13 +0800

    添加了课程索引项目

---
+++ src/components/CourseIndex.vue
@@ -0,0 +1,37 @@
+<template>
+  <li id="index">
+    <img :src="require('../'+imgsrc)" alt="" />
+    <p><slot></slot></p>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  props: ['imgsrc']
+}
+</script>
+
+<style lang="less">
+  #index {
+    float: left;
+    width: 50vw;
+    display: -webkit-flex;
+    /* Safari */
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    flex-direction: column;
+
+    img {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      align-content: space-around;
+    }
+
+    p {
+      text-align: center;
+    }
+  }
+</style>
