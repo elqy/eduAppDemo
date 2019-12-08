3b3a40456538f621d59bd85442eaa582324b5770
老刘 <liuqinyi@163.com>
2019-12-08 16:31:30 +0800

    添加了课程模块项目

---
+++ src/components/CourseModule.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="module">
+    <ul>
+      <CourseIndex imgsrc="assets/course/1.png">课程1</CourseIndex>
+    </ul>
+  </div>
+</template>
+
+<script>
+import CourseIndex from './CourseIndex.vue'
+export default {
+  components: {
+    CourseIndex
+  }
+}
+</script>
+
+<style lang="less">
+  #module {
+    ul {
+      list-style: none;
+      margin: 0.5rem 0 0 0;
+      padding: 0;
+      display: -webkit-flex;
+      /* Safari */
+      display: flex;
+      flex-direction: row;
+      flex-wrap: wrap;
+      justify-content: space-around;
+      align-items: center;
+    }
+  }
+</style>
