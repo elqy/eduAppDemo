3e797dcc31886cc364a4cf42aa1b6ef18f524e0e
李尚洁 <2845403120@qq.com>
2019-12-11 21:55:30 +0800

    修改main.js,新增vant插件

--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+import 'vant/lib/index.css'
+import { DropdownMenu, DropdownItem } from 'vant'
+Vue.use(DropdownMenu).use(DropdownItem)
+Vue.use(MintUI)
 Vue.config.productionTip = false
 
 new Vue({
